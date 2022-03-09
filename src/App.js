75655bd3e4f1fd5fdcb8e3916dd527b69e603d11
eduym17 <eduardo_yanez@live.com.mx>
2022-03-09 17:53:23 -0600

    Add tailwind tools

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App w-screen h-screen bg-black text-white">
+    que te regreses dice el linuski
+  </div>
+);
 
 export default App;
