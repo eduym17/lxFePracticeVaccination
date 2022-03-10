460b4adb92578d21d632ed71e6725aaabdd85c9f
eduym17 <eduardo_yanez@live.com.mx>
2022-03-09 22:57:47 -0600

    Add second page section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Nurse from './assets/images/nurse-blue.png';
+import Nurse from './assets/images/nurse.png';
 import Star from './assets/icons/star.png';
 import Schedule from './assets/icons/schedule.png';
 import Location from './assets/icons/location.png';
 import Calendar from './assets/icons/date_range.png';
 import Vaccines from './assets/icons/vaccines.png';
+import Astrazeneca from './assets/icons/astrazeneca.svg';
+import Pfizer from './assets/icons/pfizer.svg';
+import Moderna from './assets/icons/moderna.svg';
+import Minimize from './assets/icons/minimize.png';
+import Shield from './assets/icons/shield.png';
+import Groups from './assets/icons/groups.png';
+import Immunity from './assets/icons/immunity.png';
 
 const App = () => (
   <>
@@ -16,13 +23,13 @@ const App = () => (
         <a href="/">Education</a>
         <a href="/">News</a>
         <a href="/">Regulation</a>
-        <button href="/" type="submit" className="bg-indigo-100 text-indigo-900 font-semibold px-4 py-2 rounded-md">
+        <button href="/" type="submit" className="bg-indigo-100 text-indigo-800 font-semibold px-4 py-2 rounded-md">
           Hoax Buster
         </button>
       </div>
     </header>
     <main className="text-white">
-      <div className="bg-indigo-700 w-screen flex justify-center items-center h-[500px]">
+      <div className="bg-indigo-700 w-full flex justify-center items-center h-[500px]">
         <div className="h-full flex flex-col justify-start w-1/3 min-w-[400px] pt-12">
           <div className="text-5xl font-bold text-left pb-6 pl-16">
             <p>Fight Virus</p>
@@ -53,7 +60,7 @@ const App = () => (
               <img src={Schedule} alt="star" />
               <p>Schedule your vaccination</p>
             </div>
-            <div className="flex justify-center items-center bg-white bg-opacity-75 gap-5 p-3 rounded-md text-indigo-800">
+            <div className="flex justify-center items-center bg-white bg-opacity-75 gap-5 p-3 rounded-md text-indigo-800 shadow-indigo-900 shadow-lg">
               <div className="flex flex-col">
                 <div className="flex items-center gap-1">
                   <img src={Location} alt="location" className="bg-indigo-700 rounded-sm p-0.5" />
@@ -64,16 +71,16 @@ const App = () => (
               <div className="flex flex-col">
                 <div className="flex items-center gap-1">
                   <img src={Calendar} alt="calendar" className="bg-indigo-700 rounded-sm p-0.5" />
-                  <p>Calendar</p>
+                  <p>Date</p>
                 </div>
-                <p className="font-semibold">Jakarta Utara</p>
+                <p className="font-semibold">08 July 2021</p>
               </div>
               <div className="flex flex-col">
                 <div className="flex items-center gap-1">
                   <img src={Vaccines} alt="vaccines" className="bg-indigo-700 rounded-sm p-0.5" />
-                  <p>Vaccines</p>
+                  <p>Vaccine Type</p>
                 </div>
-                <p className="font-semibold">Jakarta Utara</p>
+                <p className="font-semibold">Moderna</p>
               </div>
               <button type="submit" className="bg-indigo-700 px-8 py-1.5 rounded-md text-white font-semibold text-lg">
                 Submit
@@ -83,7 +90,59 @@ const App = () => (
         </div>
       </div>
       <div className="bg-indigo-800">
-        algo
+        <div className="flex justify-evenly py-14">
+          <img src={Astrazeneca} alt="AstraZeneca" className="h-[70px]" />
+          <img src={Pfizer} alt="Pfizer" className="h-[70px]" />
+          <img src={Moderna} alt="Moderna" className="h-[70px]" />
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="w-2/3">
+            <p className="font-bold text-4xl pb-6">Why should I vaccine?</p>
+            <p className="font-light pb-8 w-1/2 self-start text-justify">
+              In order to avoid any doubts about getting the COVID-19 vaccine,
+              {' '}
+              identify the following 4 benefits of COVID-19 vaccination:
+            </p>
+          </div>
+          <div className="pb-14 flex gap-6">
+            <div className="bg-white w-[175px] h-[210px] p-5 flex flex-col gap-2 items-start rounded-md text-indigo-700 shadow-lg shadow-indigo-900">
+              <img src={Minimize} alt="Minimize" className="bg-indigo-700 p-1.5 rounded-md" />
+              <p className="font-semibold">Minimize the spread of viruses</p>
+              <p className="font-normal text-xs text-indigo-900">
+                Vaccine has been proven effective to prevent someone
+                {' '}
+                from getting infected with Coronavirus
+              </p>
+            </div>
+            <div className="bg-white w-[175px] h-[210px] p-5 flex flex-col gap-2 items-start rounded-md text-indigo-700 shadow-lg shadow-indigo-900">
+              <img src={Shield} alt="Shield" className="bg-indigo-700 p-1.5 rounded-md" />
+              <p className="font-semibold">Forming antibodies</p>
+              <p className="font-normal text-xs text-indigo-900">
+                COVID-19 vaccine proven to help shape antibody response
+                {' '}
+                for immune system
+              </p>
+            </div>
+            <div className="bg-white w-[175px] h-[210px] p-5 flex flex-col gap-2 items-start rounded-md text-indigo-700 shadow-lg shadow-indigo-900">
+              <img src={Groups} alt="Groups" className="bg-indigo-700 p-1.5 rounded-md" />
+              <p className="font-semibold">Protecting people nearby</p>
+              <p className="font-normal text-xs text-indigo-900">
+                The benefits of COVID-19 vaccination are the vaccine that we get can also help
+                {' '}
+                protect people around us
+              </p>
+            </div>
+            <div className="bg-white w-[175px] h-[210px] p-5 flex flex-col gap-2 items-start rounded-md text-indigo-700 shadow-lg shadow-indigo-900">
+              <img src={Immunity} alt="Immunity" className="bg-indigo-700 p-1.5 rounded-md" />
+              <p className="font-semibold">Creating groups immunity</p>
+              <p className="font-normal text-xs text-indigo-900">
+                COVID-19 vaccination is also beneficial for creating group
+                {' '}
+                immunity of herd immunity
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
     <footer>
